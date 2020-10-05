e0099c3e08611cd3cbe99c0774fefa55fa417d1c
Ryan Consylman <ryan.consylman@gmail.com>
2020-10-05 08:48:39 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require $_SERVER['DOCUMENT_ROOT']."/assets/includes/header.php";
+?>
+<main class="container">
+    <h1>PolyBlots</h1>
+    <p>
+    <svg class="bi" width="32" height="32" fill="currentColor">
+        <use xlink:href="/assets/images/bootstrap-icons.svg#shop"/>
+    </svg>
+    </p>
+</main>
+<?php
+require $_SERVER['DOCUMENT_ROOT']."/assets/includes/footer.php";
+?>
